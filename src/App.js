24720bc56af1f937f0c164f934ec43f0c04921e5
Victor Marcoci <victor.marcoci@gmail.com>
2022-04-19 14:23:22 +0200

    infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,40 +1,73 @@
-import React from 'react'
-import { useState } from 'react'
-import axios from 'axios';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  // console.log(document.documentElement.clientHeight)
-  // console.log(document.documentElement.getBoundingClientRect().bottom)
-  const [images, setImages] = useState(null);
-  const fetchData = async () => {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/photos?_limit=14&_page=1')
-    setImages(response.data)
-  }
-  fetchData()
-  // window.addEventListener('scroll', function () {
-  //   if (document.documentElement.clientHeight > document.documentElement.getBoundingClientRect().bottom - 100) { }
-  // })
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [fetching, setFetching] = useState(true);
+
+  useEffect(() => {
+    if (fetching) {
+      axios
+        .get(
+          `https://jsonplaceholder.typicode.com/photos?_limit=15&_page=${page}`
+        )
+        .then((response) => {
+          setImages([...images, ...response.data]);
+          setPage((prevState) => prevState + 1);
+        })
+        .finally(() => setFetching(false));
+    }
+  }, [fetching]);
+
+  useEffect(() => {
+    document.addEventListener("scroll", scroll);
+
+    return function () {
+      document.removeEventListener("scroll", scroll);
+    };
+  }, []);
+
+  const scroll = (e) => {
+    if (
+      e.target.documentElement.scrollHeight -
+        (e.target.documentElement.scrollTop + window.innerHeight) <
+      100
+    ) {
+      setFetching(true);
+    }
+  };
+  // const fetchData = async () => {
+  //   const response = await axios.get(
+  //     "https://jsonplaceholder.typicode.com/photos?_limit=14&_page=2"
+  //   );
+
+  //   setImages(response.data);
+  // };
+
+  // fetchData();
+
+  // const addElements = () => {
+  //   this.fetchData();
+  //   console.log("added");
+  // };
+
   return (
-    <div className='App'>
+    <div className="App">
       <h1>Photo Gallery</h1>
-      <div>
-        {/* <button className="btn" onClick={fetchData}>Click</button> */}
-      </div>
-      <div className='images' id='img'>
-        {
-          images && images.map((img, index) => {
-            return (
-              <div className='image' key={img.id}>
-                <img src={img.url}></img>
-                <div className='info'>
-                  <h3>Image {index + 1}</h3>
-                  <p>Title: {img.title}</p>
-                </div>
+      <div className="images">
+        {images.map((img, index) => {
+          return (
+            <div className="image" key={img.id}>
+              <img src={img.url}></img>
+              <div className="info">
+                <h3>Image {index + 1}</h3>
+                <p>Title: {img.title}</p>
               </div>
-            )
-          })
-        }
+            </div>
+          );
+        })}
       </div>
     </div>
   );
